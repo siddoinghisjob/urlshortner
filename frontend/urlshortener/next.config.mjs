2bc5b75adf6ef2846bfdd834b2af37e85392a03a
Soumya Deep Sarkar <96379749+siddoinghisjob@users.noreply.github.com>
2024-05-20 22:11:08 +0530

    made changes

--- frontend/urlshortener/next.config.mjs
+++ frontend/urlshortener/next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     env :{
-        BACKEND_URL : "https://urlshortner-p173.onrender.com/",
-        FRONTEND_URL : "https://urlshortner-brgq.vercel.app/"
+        BACKEND_URL : "https://urlshortner-p173.onrender.com",
+        FRONTEND_URL : "https://urlshortner-brgq.vercel.app"
     }
 };
 
